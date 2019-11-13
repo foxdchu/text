5a3bd5d6bb88fa738a9752d4381e556dc34aee9e
foxdchu <47610780+foxdchu@users.noreply.github.com>
2019-11-13 14:55:04 -0800

    uploaded files

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="Attention", 
+    version="0.1",
+    author="Mark Rabins",
+    description="A small package for Mark",
+    url="https://github.com/foxbroadcasting/Attention",
+    packages=['Attention']
+)
